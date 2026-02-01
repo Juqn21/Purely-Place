b1bf0534dee4c6d8d7a68ae2ffbf8e6f993254a3
JuqnSK <juanandresfg180@gmail.com>
2026-02-01 15:37:13 -0400

    Final Struct Commit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # main.py
-from src.game import MiJuego  # <--- ESTA ES LA LÍNEA QUE FALTA
+from src.game import MiJuego  
 
 if __name__ == "__main__":
-    # Se instancia y se corre. 
-    # El Core ya sabe qué hacer porque la lógica está en la base.
+    
     game = MiJuego()
     game.run_preview()
